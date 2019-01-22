e3b006371ffe375dcc05008cd215bcc54a887894
Kevin Griego <kev.gri32@gmail.com>
2019-01-21 21:05:13 -0700

    changed post url

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -43,7 +43,7 @@ class ContactForm extends Component {
       typeOfBusniess
     };
     e.preventDefault();
-    fetch("/", {
+    fetch("/ContactUs/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", ...DataToSend })
