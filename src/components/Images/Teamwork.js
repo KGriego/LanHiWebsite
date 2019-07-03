9edcaed80cfb2ad73d810a4ccb76850bd2373260
Kevin Griego <kev.gri32@gmail.com>
2019-07-03 16:42:06 -0700

    fucking typo

--- src/components/Images/Teamwork.js
+++ src/components/Images/Teamwork.js
@@ -17,7 +17,7 @@ const Image = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "teamwork.jpg" }) {
+        placeholderImage: file(relativePath: { eq: "teamWork.jpg" }) {
           childImageSharp {
             fluid(maxWidth: 300) {
               ...GatsbyImageSharpFluid
